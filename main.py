7bd186183a56e66bc2a0621cd1f684d08d9361eb
Reiter21 <palash21gupta@gmail.com>
2022-04-25 16:46:18 +0530

    fixed beeping

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import imutils
 import time
 import cv2
 from playsound import playsound
+from colorama import Fore, Style
 
 
 ap = argparse.ArgumentParser()
@@ -17,10 +18,10 @@ ap.add_argument("-c", "--confidence", type=float, default=0.2,
 	help="minimum probability to filter weak predictions")
 args = vars(ap.parse_args())
 
-CLASSES = ["aeroplane", "background", "bicycle", "bird", "boat",
-           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-           "sofa", "train", "tvmonitor"]
+CLASSES = ["_", "background", "_", "bird", "_",
+           "_", "_", "_", "cat", "_", "cow", "_",
+           "dog", "horse", "_", "person", "_", "sheep",
+           "_", "_", "_"]
 
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
@@ -28,22 +29,28 @@ print("[INFO] loading model...")
 net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
+vs = VideoStream(src=1).start()
 
 # warm up the camera for a couple of seconds
-time.sleep(2.0)
+#time.sleep(2.0)
 
 fps = FPS().start()
 
 soundCounter = 0
 soundCounterSpeed = 45
+personVisibleFor = 0
+framesElapsedSincePerson = 1000
 
 while True:
 	soundCounter += 1
+	framesElapsedSincePerson += 1
+
+	if framesElapsedSincePerson >= 100:
+		personVisibleFor = 0
 
 	frame = vs.read()
 	frame = imutils.resize(frame, width=800)
-	print(frame.shape) # (225, 400, 3)
+	#print(frame.shape) # (225, 400, 3)
 
 	(h, w) = frame.shape[:2]
 
@@ -69,15 +76,28 @@ while True:
 			
 			(startX, startY, endX, endY) = box.astype("int")
 
-			label = f"{CLASSES[idx]}: {round(confidence * 100, 2)}"
+			label = CLASSES[idx]
 
 			# alarm system
-			if CLASSES[idx] == 'person' and soundCounter >= soundCounterSpeed:
+			if CLASSES[idx] == 'person' and not personVisibleFor >= 75:
+				personVisibleFor += 1
+
+				if personVisibleFor == 1 and framesElapsedSincePerson >= 100:
+					playsound('beep-02.mp3', False)
+					print(Fore.CYAN + "bad beep")
+				
+				framesElapsedSincePerson = 0
+
+
+			if CLASSES[idx] == 'person' and soundCounter >= soundCounterSpeed and personVisibleFor >= 25:
 				playsound('beep-02.mp3', False)
-				soundCounter -= soundCounterSpeed
+				print(Fore.GREEN + "good beep")
+				soundCounter = 0
 
+			#print(CLASSES[idx], idx)
 
-			print("Object detected: ", label)
+			if label == "person":
+				print(Fore.RED + "PERSON DETECTED")
 
 			# bounding box
 			cv2.rectangle(frame, (startX, startY), (endX, endY),
@@ -85,7 +105,7 @@ while True:
 			y = startY - 15 if startY - 15 > 15 else startY + 15
 
 			cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-
+		
 	cv2.imshow("Frame", frame)
 
 	key = cv2.waitKey(1) & 0xFF
